18199f15727573be6288983903b2bac382cc67e6
cort zhang <learn_zhang@163.com>
2020-08-06 18:17:50 +0800

    add project to repository

---
+++ readme.txt
@@ -0,0 +1,5 @@
+Git is a distributed version control system.
+Git is free software distributed under the GPL.
+Git has a mutable index called stage.
+Git tracks changes of files.
+201411251129 reating a new branch is quick & simple
---
+++ src/main/java/com/cort/App.java
@@ -0,0 +1,23 @@
+package com.cort;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println("bbbbb");
+
+        //fkaspojegeagme
+        System.out.println("hahdhfe");
+        System.out.println("this is my first maven project");
+        System.out.println("again");
+    }
+}
+/*
+    解决IDEA push失败的原因
+    打开Git Bash，然后进入相应的目录，然后敲git命令：
+        $ git pull origin master --allow-unrelated-histories*/
