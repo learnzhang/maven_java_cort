66303256f6b8d2ebc888e7b364edc5533ce4d9fd
cort zhang <learn_zhang@163.com>
2020-08-06 18:19:45 +0800

    update main function

--- src/main/java/com/cort/App.java
+++ src/main/java/com/cort/App.java
@@ -8,10 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-
+        System.out.println("Hello World");
         System.out.println("bbbbb");
-
-        //fkaspojegeagme
         System.out.println("hahdhfe");
         System.out.println("this is my first maven project");
         System.out.println("again");
